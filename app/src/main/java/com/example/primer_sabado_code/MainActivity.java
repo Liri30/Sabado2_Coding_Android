e66b7779c03efe1c5d9fcac4615ede83773a3f04
manuelliriano.ml@gmail.com <turkeylo15>
2019-10-19 18:00:38 -0400

    First Commit

--- app/src/main/java/com/example/primer_sabado_code/MainActivity.java
+++ app/src/main/java/com/example/primer_sabado_code/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button button, button2;
     EditText myEditText;
@@ -18,10 +18,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     button =findViewById(R.id.button);
-     button.setOnClickListener(this);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(this);
 
-        button2 =findViewById(R.id.button2);
+        button2 = findViewById(R.id.button2);
         button2.setOnClickListener(this);
 
 
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void presionar(View view) {
         myEditText = findViewById(R.id.myEditText);
         String input = String.valueOf(myEditText.getText());
-        Toast.makeText(this,input,Toast.LENGTH_LONG).show();
+        Toast.makeText(this, input, Toast.LENGTH_LONG).show();
     }
 
     //este metodo se llamar con el listener en la clase, para eso se implementa el metodo OnClickListener de la clase View
@@ -41,11 +41,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         myEditText = findViewById(R.id.myEditText);
         String input = String.valueOf(myEditText.getText());
 
-        if (v.getId()== R.id.button)
-        Toast.makeText(this,input +"Button Entrar",Toast.LENGTH_LONG).show();
+        if (v.getId() == R.id.button)
+            Toast.makeText(this, input + "Button Entrar", Toast.LENGTH_LONG).show();
 
         else if (v.getId() == R.id.button2)
-            Toast.makeText(this,input +"Button Otra vez",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, input + "Button Otra vez", Toast.LENGTH_LONG).show();
 
     }
 }
